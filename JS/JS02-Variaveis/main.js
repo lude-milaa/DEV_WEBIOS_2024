06e928b180b55dab63e229c812499da5152a09c2
lude-milaa <ludemila714@gmail.com>
2024-05-08 21:44:40 -0300

    js02

---
+++ JS/JS02-Variaveis/main.js
@@ -0,0 +1,59 @@
+//Objeto console, as aspas são para texto
+console.log("Hello World")
+
+//variaveis
+//quero criar um espaço na memoria do computador chamado _felippe e dizer que esse espaço na memoria irá receber o valor 8
+
+var _felippe =8
+_felippe = "Messi"  //redeclarar uma variavel
+
+let LetsGo = 4 // não pode redeclarar 
+
+console.log(LetsGo)
+
+console.clear()
+
+//const
+const PI = 3.1415;
+
+console.log(PI)
+
+console.clear()
+
+let NAN = 0/0;
+
+console.log(NAN)
+
+console.clear()
+
+let corinthians = "Melhor time, palmeiras sem mundial" // recebeu uma strings
+
+console.log(corinthians)
+
+console.clear()
+
+let IOS = "Instituto da Oportunidade Social" // variavel recebeu uma strings
+
+console.log("Instituto" + "da" + "Oportunidade" + "Social");
+
+var numero1 = 2
+var texto = "Eu tenho"
+var texto2 = "Reais"
+
+console.log(texto + numero1 + texto2)
+//concatenar = é possivel concatenar strings ou concatenar variaveis
+
+console.clear()
+
+//valores booleanos
+
+var num1 = 5
+var num2 = 5
+// num1 é igual a num2
+// 1 é igual a 1 = true
+
+console.log(num1 == num2)
+
+var teste
+
+console.log(teste)
