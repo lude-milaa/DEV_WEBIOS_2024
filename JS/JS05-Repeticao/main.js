32e05e4aefb4a8eed71bcc281d4e7b8633bc6176
lude-milaa <ludemila714@gmail.com>
2024-05-15 21:41:47 -0300

    aula js5

---
+++ JS/JS05-Repeticao/main.js
@@ -0,0 +1,41 @@
+// white = enquanto
+// enquanto 0 < 10 vou apresentar a mensagem "0 ainda é menor que 10"
+
+var contador = 0
+
+// while(contador <= 10){   // while verifica se é verdadeiro 
+// console.log(contador + "0 ainda é menor que 10")
+// debugger // é um ponto de parada
+// contador++
+// }
+
+// do while primeiro executa o codigo pelo menos uma vez e depois verrifica se é verdadeiro ou falso
+
+var contador = 10
+
+// do{
+//     debugger
+//     console.log(contador + "0 ainda é menor que 10")
+//     contador++;
+// }while(contador < 10)
+
+while(contador <= 10){   // while verifica se é verdadeiro 
+     console.log(contador + "0 ainda é menor que 10")
+     contador++
+}
+
+for(var contador = 10; contador < 10 && contador >= 5 || contador == 90; contador ++){
+
+}
+
+
+// exemplo da lição de casa
+if{
+    for(){
+
+    }
+}else{
+    while(){
+
+    }
+} // 
